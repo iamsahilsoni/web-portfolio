f54a5246356b0fe316f513d5dd9cd1e6aa587114
iamsahilsoni <sonisahil12345@gmail.com>
2023-01-31 00:55:26 -0500

    add boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
+import { RightPanelComponent } from './right-panel/right-panel.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftPanelComponent,
+    RightPanelComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
